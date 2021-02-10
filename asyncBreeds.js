b042ec1cfbc028a6a8cc25aeab9d153541c0f629
Kevin Nicolas <bonjour@kevinnicolas.com>
2021-02-10 01:04:14 +0000

    add test for async

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -1,11 +1,15 @@
 const fs = require("fs");
 
-const breedDetailsFromFile = function(breed, functionToRunWhenThingsAreDone) {
+const breedDetailsFromFile = function(breed, done) {
   console.log("breedDetailsFromFile: Calling readFile...");
   fs.readFile(`./data/${breed}.txt`, "utf8", (error, data) => {
     // CHANGE: Pass data into callback instead of returning it directly
     console.log("In readFile's Callback: it has the data.");
-    if (!error) functionToRunWhenThingsAreDone(data);
+    if (!error) {
+      done(data);
+    } else {
+      done(undefined);
+    }
   });
 };
 
@@ -14,5 +18,4 @@ const printOutCatBreed = (breed) => {
   console.log("Return Value: ", breed); // => print out details correctly.
 };
 
-// CHANGE 2: we're now passing two arguments into breedDetailsFromFile: breed string and a callback function
-breedDetailsFromFile("Bombay", printOutCatBreed);
+module.exports = breedDetailsFromFile;
